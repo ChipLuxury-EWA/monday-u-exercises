8f0986e0537b171961138a7d08cdec1d812f2a1a
Tom Portugez <chip_luxury@gmail.com>
2022-05-17 16:48:19 +0300

    faet: working 'adding task'

--- src/ex1/script.js
+++ src/ex1/script.js
@@ -1,1 +1,38 @@
 //SELECTORS:
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event listeners:
+// todoButton.addEventListener('click', addTodo)
+todoButton.onclick = addTodo;
+
+//Functions:
+
+function addTodo(event) {
+    event.preventDefault();
+
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //create a new todo list item:
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    //Check-mark button:
+    const completeButton = document.createElement("button");
+    completeButton.innerHTML = '<i class="fas fa-check"></i>';
+    completeButton.classList.add("complete-button");
+    todoDiv.appendChild(completeButton);
+
+    //check trash button:
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-button");
+    todoDiv.appendChild(trashButton);
+
+    todoList.appendChild(todoDiv);
+    todoInput.value = ""
+}
