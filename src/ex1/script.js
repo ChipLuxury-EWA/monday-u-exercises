a6bf59eaa85e1a19092e5448cbf9b17f237030fb
Tom Portugez <chip_luxury@gmail.com>
2022-05-18 23:22:57 +0300

    build: 2.0.0

--- src/ex1/script.js
+++ src/ex1/script.js
@@ -2,12 +2,16 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
-const filterOption = document.querySelector(".filter-todo");
+const todos = todoList.childNodes;
+const clearAllButton = document.querySelector(".clear-all-button");
+// const filterOption = document.querySelector(".filter-todo");
+const pendingCounter = document.querySelector(".pending-counter");
 
 //Event listeners:
 todoButton.onclick = addTodo;
 todoList.onclick = deleteOrCompleteTask;
-filterOption.onclick = filterTodo;
+clearAllButton.onclick = clearAllTasks;
+// filterOption.onclick = filterTodo;
 //Functions:
 
 function addTodo(event) {
@@ -18,11 +22,12 @@ function addTodo(event) {
 
     //create a new todo list item:
     const newTodo = document.createElement("li");
-    if(todoInput.value === "") {
-        alert("ERROR - no empty task is allowed")
-        return
+
+    if (todoInput.value === "") {
+        alert("ERROR - no empty task is allowed");
+        return;
     }
-    
+
     newTodo.innerText = todoInput.value;
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
@@ -55,12 +60,11 @@ function deleteOrCompleteTask(event) {
         const todo = item.parentElement;
         todo.classList.toggle("completed");
     } else {
-        confirm(item.innerHTML)
+        confirm(item.innerHTML);
     }
 }
 
 function filterTodo(event) {
-    const todos = todoList.childNodes;
     todos.forEach(function (todo) {
         switch (event.target.value) {
             case "completed":
@@ -83,3 +87,43 @@ function filterTodo(event) {
         }
     });
 }
+
+function clearAllTasks() {
+    const allTodos = document.querySelectorAll('.todo')
+    allTodos.forEach(task => task.remove())
+}
+
+function updatePending() {
+    const completedTask = document.querySelectorAll(".completed");
+    const pendingTasksAmount = todos.length - completedTask.length;
+    if (pendingTasksAmount === 0) {
+        pendingCounter.innerHTML = `YAYY all your task are done, add new task...`;
+    } else {
+        pendingCounter.innerHTML = `You have ${pendingTasksAmount} pending tasks`;
+    }
+    showHoseClearAllButton();
+}
+
+function showHoseClearAllButton() {
+    if (todos.length !== 0) {
+        clearAllButton.style.display = "flex";
+    } else {
+        clearAllButton.style.display = "none";
+    }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver:
+const config = { attributes: true, childList: true, subtree: true };
+
+const callback = function (mutationsList, observer) {
+    for (const mutation of mutationsList) {
+        if (mutation.type === "childList") {
+            updatePending();
+        } else if (mutation.type === "attributes") {
+            updatePending();
+        }
+    }
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(todoList, config);
