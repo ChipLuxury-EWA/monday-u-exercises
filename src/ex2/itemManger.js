433829bc8df6b3872370b92ee1ae1b02972c2fe5
Tom Portugez <chip_luxury@gmail.com>
2022-05-24 21:14:19 +0300

    feat:class refactoring-added delteOrComplte func

--- src/ex2/itemManger.js
+++ src/ex2/itemManger.js
@@ -2,54 +2,16 @@ export class ItemManger {
     constructor(params) {
         
     }
+}
 
-    addTodo(event) {
-        event.preventDefault();
-
-        if (this.todoInput.value === "") {
-            alert("ERROR - no empty task is allowed");
-            return;
-        }
-        // if (regex just numbers) {fetch pokemon by id}
-        this.taskItem = this.createTodoDiv(this.todoInput.value);
-        this.todoList.appendChild(this.taskItem);
-        // clearing the input:
-        this.todoInput.value = "";
+class ItemManager2 {
+    constructor(){
+        this.items = [];
     }
 
-    createTodoDiv(todoInput) {
-        const newTodoDiv = document.createElement("div");
-        newTodoDiv.classList.add("todo");
-        newTodoDiv.appendChild(this.createTodoTitle(todoInput));
-        newTodoDiv.append(...this.createTwoButton(this));
-        return newTodoDiv;
-    }
+    remove(){
+    }_
 
-    createTodoTitle(todoTitle) {
-        // this function create a new todo title and assign a class
-        const newTodo = document.createElement("li");
-        newTodo.innerText = todoTitle;
-        newTodo.classList.add("todo-item");
-        return newTodo;
-    }
-
-    createTwoButton() {
-        // This function create 2 necessary buttons:
-        // according to font awesome buttonIcon can be "check" or "trash"
-
-        //Check-mark button:
-        const button1 = this.createButton("complete-button", "check");
-        //check trash button:
-        const button2 = this.createButton("trash-button", "trash");
-        return [button1, button2];
-    }
-
-    createButton(buttonClass, buttonIcon) {
-        // This function create a button with font-awesome icon
-        // and add class to it
-        const newButton = document.createElement("button");
-        newButton.innerHTML = `<i class="fas fa-${buttonIcon}" ></i>`;
-        newButton.classList.add(buttonClass);
-        return newButton;
+    add(){
     }
 }
--- src/ex2/main.js
+++ src/ex2/main.js
@@ -1,9 +1,70 @@
 import { ItemManger } from "./itemManger.js";
+
 class Main {
-    constructor(ItemManger) {
-        this.addTodo = ItemManger.addTodo.bind(this)
-        this.createTodoDiv = ItemManger.createTodoDiv.bind(this)
-    }
+    addTodo = (event) => {
+        console.log(this);
+        event.preventDefault();
+
+        if (this.todoInput.value === "") {
+            alert("ERROR - no empty task is allowed");
+            return;
+        }
+        // if (regex just numbers) {fetch pokemon by id}
+        this.taskItem = this.createTodoDiv(this.todoInput.value);
+        this.todoList.appendChild(this.taskItem);
+        // clearing the input:
+        this.todoInput.value = "";
+    };
+
+    createTodoDiv = (todoInput) => {
+        const newTodoDiv = document.createElement("div");
+        newTodoDiv.classList.add("todo");
+        newTodoDiv.appendChild(this.createTodoTitle(todoInput));
+        newTodoDiv.append(...this.createTwoBasicButtons(this));
+        return newTodoDiv;
+    };
+
+    createTodoTitle = (todoTitle) => {
+        // this function create a new todo title and assign a class
+        const newTodo = document.createElement("li");
+        newTodo.innerText = todoTitle;
+        newTodo.classList.add("todo-item");
+        return newTodo;
+    };
+
+    createTwoBasicButtons = () => {
+        // according to font awesome buttonIcon are "check" or "trash"
+        const button1 = this.createButtonAndAddClass(
+            "complete-button",
+            "check"
+        );
+        const button2 = this.createButtonAndAddClass("trash-button", "trash");
+        return [button1, button2];
+    };
+
+    createButtonAndAddClass = (buttonClass, buttonIcon) => {
+        const newButton = document.createElement("button");
+        //i tag is for font awesome
+        newButton.innerHTML = `<i class="fas fa-${buttonIcon}" ></i>`;
+        newButton.classList.add(buttonClass);
+        return newButton;
+    };
+
+    deleteOrCompleteTask = (event) => {
+        const item = event.target;
+        if (item.classList[0] === "trash-button") {
+            const todo = item.parentElement;
+            todo.classList.add("fall");
+            todo.addEventListener("transitionend", function () {
+                todo.remove();
+            });
+        } else if (item.classList[0] === "complete-button") {
+            const todo = item.parentElement;
+            todo.classList.toggle(TASK_COMPLETED_CLASS_NAME);
+        } else if (item.classList[0] === "todo-item") {
+            confirm(item.innerHTML);
+        }
+    };
 
     init() {
         //SELECTORS:
@@ -16,18 +77,17 @@ class Main {
         this.pendingCounter = document.querySelector(".pending-counter");
 
         //Event listeners:
-        this.todoButton.onclick = this.addTodo.bind(this);
-        // this.todoList.onclick = deleteOrCompleteTask;
-        // clearAllButton.onclick = clearAllTasks;
-        // filterOption.onclick = filterTodo;
+        this.todoButton.onclick = this.addTodo;
+        this.todoList.onclick = this.deleteOrCompleteTask;
+        // clearAllButton.onclick = this.clearAllTasks;
+        // filterOption.onclick = this.filterTodo;
     }
 }
 
-const item = new ItemManger();
-const main = new Main(item);
+const main = new Main();
 document.addEventListener("DOMContentLoaded", function () {
     // you should create an `init` method in your class
     // the method should add the event listener to your "add" button
-    
+
     main.init();
 });
