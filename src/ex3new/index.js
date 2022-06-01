444b297fa4028dab435f1f862145d173719b5b1c
Tom Portugez <chip_luxury@gmail.com>
2022-06-01 09:36:40 +0300

    feat: command add - help & cli input

--- src/ex3new/index.js
+++ src/ex3new/index.js
@@ -6,7 +6,13 @@ const program = new Command();
 
 program.name("Todo App V2").description("The best Todo app!").version("2.0.0");
 
-
+program
+    .command("add")
+    .description("Add a new task, or enter id to catch pokemon")
+    .argument("<string or number>", "task title, or id of pokemon")
+    .action((cliInput) => {
+        console.log(cliInput)
+    });
 
 
 program.parse();
