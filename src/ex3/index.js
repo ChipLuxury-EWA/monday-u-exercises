2b67b0ddb8e796b72b20018afdb4173ab3a9df29
Tom Portugez <chip_luxury@gmail.com>
2022-06-01 09:03:37 +0300

    working app

--- src/ex3/index.js
+++ src/ex3/index.js
@@ -1,5 +1,15 @@
-import { myLogger, readLog } from "mondayu-logger-tom-portugez";
+#!/usr/bin/env node
+import {
+    myLogger,
+    readLog,
+    readLogLineByLine,
+    writeArrayOfLinesToLog,
+} from "mondayu-logger-tom-portugez";
 import { Command } from "commander";
+import chalkAnimation from "chalk-animation";
+import { createSpinner } from "nanospinner";
+import chalk from "chalk";
+
 import { PokemonClient } from "./services/pokemonClient.js";
 import inquirer from "inquirer";
 
@@ -13,16 +23,17 @@ program
     .argument("<string>", "task title, or id of pokemon")
     .action((taskTitle) => {
         addTodo(taskTitle);
+        readAndPrintAllTodos();
     });
 
 program
     .command("get")
     .description("Get all your task")
-    .action(async () => {
-        console.log("getting all of your todos:");
+    .action(() => {
         try {
-            console.log(await readLog(), "\n^^ all your todos ^^");
+            readAndPrintAllTodos();
         } catch (error) {
+            console.log(error);
             console.log(`error fetching all of your todos -- see error logos`);
             myLogger.error("error fetching all todos\n\n", error, "\n");
         }
@@ -31,14 +42,49 @@ program
 program
     .command("delete")
     .description("Delete specific task by index")
-    .argument("<number>", "Task index from the list")
-    .action((index) => {
-        console.log(`Deleting title number ${index}`);
+    .argument("<number>", "Todo number from the list")
+    .action((todoNumber) => {
+        deleteTodo(todoNumber);
     });
+////////////////////////////////
+const deleteTodo = async (todoNumber) => {
+    const index = todoNumber - 1;
+    const file = await readLogLineByLine();
+    if (todoNumber > file.length) {
+        console.log(`error - incorrect index`);
+        return;
+    }
+    printLineWithStrikeThrow(file, index);
+    console.log(`deleting #${file[index]}`);
+    file.splice(index, 1);
+    await writeArrayOfLinesToLog(file);
+    readAndPrintAllTodos();
+};
 
-const addTodo = (userInput) => {
+const printLineWithStrikeThrow = (todosArray, index) => {
+    todosArray.forEach((todo, i) => {
+        if (i === index) {
+            console.log(chalk.strikethrough(`${i + 1}) ${todo}.`));
+        } else {
+            console.log(`${i + 1}) ${todo}.`);
+        }
+    });
+};
+
+const readAndPrintAllTodos = async () => {
+    const todos = await readLogLineByLine();
+    printAllTodosFromArray(todos);
+};
+
+const printAllTodosFromArray = async (todosArray) => {
+    todosArray.forEach((todo, index) => {
+        todo && console.log(`${index + 1}) ${todo}.`);
+    });
+};
+
+const addTodo = async (userInput) => {
     if (!isNaN(userInput)) {
-        const pokemonName = pokemonClient.getPokemonNameById(userInput);
+        const pokemonName = await pokemonClient.getPokemonNameById(userInput);
         exportTaskToConsoleAndLog(`catch ${pokemonName}`);
     } else {
         exportTaskToConsoleAndLog(userInput);
