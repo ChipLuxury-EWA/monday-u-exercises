e5cf1db6bbd9f313b0ad49686043ea1d2e1bcdfc
Tom Portugez <chip_luxury@gmail.com>
2022-06-23 22:02:30 +0300

    feat: done timestamp works

--- src/ex5/dist/clients/item_client.js
+++ src/ex5/dist/clients/item_client.js
@@ -14,10 +14,17 @@ class ItemClient {
     };
 
     updateItem = async (item) => {
+        
+        const bodyMessage = {};
+        bodyMessage.id = item.id;
+        bodyMessage.status = !item.status;
+        if (!item.status) bodyMessage.done = new Date(); // send done date just when mark task done (*false* -> true)
+        bodyMessage.taskName = item.taskName
+
         await fetch("/item", {
             method: "PUT",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ id: item.id, status: !item.status }),
+            body: JSON.stringify(bodyMessage),
         });
     };
 
