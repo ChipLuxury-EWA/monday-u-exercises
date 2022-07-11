e6de3c2772cba654e1ff3aa6554a027867dc9df7
Tom Portugez <chip_luxury@gmail.com>
2022-07-12 02:33:21 +0300

    dev: follow along redux toolkit quick-start

---
+++ client-no-storybook/src/redux/task.slice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {
+    getItems,
+    postItem,
+    updateItem,
+    deleteItem,
+} from "../services/item_client";
+
+export const taskSlice = createSlice({
+    name: taskSlice,
+    initialState: { value: [] },
+    reducers: {
+        getAllTasks: async (state) => {
+            state.value = await getItems();
+        },
+        addTask: async (state, action) => {
+            postItem(action.payload);
+            state.value = await getItems();
+        },
+        updateTask: async (state, action) => {
+            updateItem(action.payload);
+            state.value = await getItems();
+        },
+        deleteTask: async (state, action) => {
+            deleteItem(action.payload);
+            state.value = await getItems();
+        },
+    },
+});
+
+export const { getAllTasks, addTask, updateTask, deleteTask } =
+    taskSlice.actions;
+export default taskSlice.reducer;
