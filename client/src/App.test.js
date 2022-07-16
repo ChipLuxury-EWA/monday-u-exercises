4d5db6d528f85cac8cc566aed55f17db76f19586
Tom Portugez <chip_luxury@gmail.com>
2022-07-16 15:19:21 +0300

    test: app component 5 tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,15 +1,51 @@
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
+
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
-test("renders learn react link", () => {
+const TASK_FOR_TEST = "jest";
+beforeEach(() => {
+
     render(
         <Provider store={store}>
             <App />
         </Provider>
     );
+    // screen.debug()
+});
 
-    const linkElement = screen.getByText(/Todo app/i);
-    expect(linkElement).toBeInTheDocument();
+test("check app rendering by catching head title", () => {
+    expect(screen.getByText(/Todo app/i)).toBeInTheDocument();
+});
+
+test('The selected tab should be "Tasks"', () => {
+    const tasksTab = screen.getByRole("tab", { selected: true });
+    expect(tasksTab).toHaveTextContent(/tasks/i);
+});
+
+test('The unselected tab should not be "Tasks" ', () => {
+    const aboutTab = screen.getByRole("tab", { selected: false });
+    expect(aboutTab).not.toHaveTextContent(/tasks/i);
+});
+
+test("the user can type task to input", () => {
+    const input = screen.getByPlaceholderText(/Add a new task/i);
+    userEvent.type(input, TASK_FOR_TEST);
+    expect(input.value).toEqual(TASK_FOR_TEST);
+    // expect(input.value).toEqual(TASK_FOR_TEST+"foo")
+});
+
+test('clicking on the "+" icon clears the input', () => {
+    const input = screen.getByPlaceholderText(/Add a new task/i);
+    userEvent.type(input, TASK_FOR_TEST);
+
+    const addButton = document.querySelector(
+        ".main > div:nth-child(2) > div:nth-child(2) > div > div > div:nth-child(2) > svg"
+    );
+    userEvent.click(addButton);
+    expect(input.value).toEqual("");
+    // expect(input.value).toEqual("foo")
 });
