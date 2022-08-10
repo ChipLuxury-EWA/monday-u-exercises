b15ca0ef050d3255eb711c4c59bef0e4e87c0483
Tom Portugez <chip_luxury@gmail.com>
2022-08-10 17:38:30 +0300

    heroku deployment

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const path = require("path");
 const api = require("./server/routes/api");
 
+process.env.PWD = process.cwd()
+
+
 const main = async () => {
     const app = express();
     app.use(express.json());
 
-    app.use(express.static(path.join(__dirname, "./client/build")));
+    app.use(express.static(path.join(process.env.PWD+ "/client/build")));
     app.use("/", api);
 
     const port = process.env.PORT || "3042";
