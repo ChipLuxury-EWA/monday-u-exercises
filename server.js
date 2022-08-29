02da0091e8ec80713543977b689ca6d92eb31be2
Tom Portugez <chip_luxury@gmail.com>
2022-08-29 22:40:40 +0300

    heroku deployment

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 const express = require("express");
 const path = require("path");
 const api = require("./server/routes/api");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
-dotenv.config()
+dotenv.config();
 
-process.env.PWD = process.cwd()
+process.env.PWD = process.cwd();
 
+const app = express();
+app.use(express.json());
 
-const main = async () => {
-    const app = express();
-    app.use(express.json());
+// app.use(express.static(path.join(process.env.PWD+ "/client/build")));
+app.use("/", api);
 
-    app.use(express.static(path.join(process.env.PWD+ "/client/build")));
-    app.use("/", api);
-
-    const port = process.env.PORT || "3042";
-    app.listen(port, function () {
-        console.log("Running on " + port);
-    });
-};
-
-main();
+const port = process.env.PORT || "3042";
+app.listen(port, function () {
+    console.log("Running on " + port);
+});
